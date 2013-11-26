6b5262f31977bb4089c011ba0c47a394deaa4f2c
= <=>
2013-11-26 13:39:54 +0800

    changed master.

---
+++ new_on_master.h
@@ -0,0 +1,32 @@
+#ifndef _NEW_ON_MASTER_H_
+#define _NEW_ON_MASTER_H_
+
+#include <boost/shared_ptr.hpp>
+
+namespace LSW_{
+  
+  /**
+   * @class new_on_master
+   * 
+   */
+  class new_on_master{
+	
+  public:
+	new_on_master(){}
+	
+  protected:
+	
+  private:
+	
+  };
+  
+  typedef boost::shared_ptr<new_on_master> pnew_on_master;
+  
+}//end of namespace
+
+#endif /*_NEW_ON_MASTER_H_*/
+
+#include "new_on_master.h"
+
+using namespace LSW_;
+
